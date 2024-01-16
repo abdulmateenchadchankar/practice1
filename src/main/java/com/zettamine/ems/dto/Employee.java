021d5dfa39907c7ea4871285f3ee98524c849928
abdul <aachadchankar@gmail.com>
2024-01-16 15:02:57 +0530

    Dto layer developed by chris

---
+++ src/main/java/com/zettamine/ems/dto/Employee.java
@@ -0,0 +1,20 @@
+package com.zettamine.ems.dto;
+
+public class Employee {
+	private Integer empNo;
+	private String empName;
+	public Integer getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(Integer empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+
+}
